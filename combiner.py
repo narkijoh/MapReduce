5d6fdd0a76ab5c0346cd91b0f48eb2d2bb4f7093
narkijoh <84466752+narkijoh@users.noreply.github.com>
2021-05-21 10:10:59 +0300

    Add files via upload

---
+++ combiner.py
@@ -0,0 +1,24 @@
+import sys
+
+cur_station = ""
+station_count = 0
+sum_temps = 0
+
+for line in sys.stdin:
+    try:
+        line = line.split('\t')
+        station, temp = line[0], float(line[1])
+
+        if station != cur_station:
+            if cur_station != "":
+                print("%s\t%s\t%s"%(cur_station, sum_temps, station_count))
+            cur_station = station
+            station_count = 1
+            sum_temps = temp
+        else:
+            station_count += 1
+            sum_temps += temp
+    except:
+        pass
+        
+print("%s\t%s\t%s"%(cur_station, sum_temps, station_count))
